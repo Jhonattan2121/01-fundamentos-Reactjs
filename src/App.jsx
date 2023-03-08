6d845548e5f6d1758afaadccd0f47dfdcab075da
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-03-07 23:24:38 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import {Post} from "./Post";
+import { Header } from "./components/Header";
+
+import './style.css'
 
 export function App() {
   return (
     <div>
+      <Header />
     <Post 
     author="Jhonattan ferri" 
     content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum consectetur fugiat aperiam tenetur ipsa! Consequuntur temporibus mollitia assumenda minus culpa excepturi necessitatibus quas nulla animi? Velit delectus rem exercitationem sed!" />
