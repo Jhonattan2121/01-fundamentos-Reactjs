95fbb38608fb6b189848303d01ee943ea05cee15
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-03-08 02:25:12 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import {Post} from "./Post";
 import { Header } from "./components/Header";
-
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-    <Post 
-    author="Jhonattan ferri" 
-    content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum consectetur fugiat aperiam tenetur ipsa! Consequuntur temporibus mollitia assumenda minus culpa excepturi necessitatibus quas nulla animi? Velit delectus rem exercitationem sed!" />
-    <Post 
-    author="Claro show"
-    content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum consectetur fugiat aperiam tenetur ipsa! Consequuntur temporibus mollitia assumenda minus culpa excepturi necessitatibus quas nulla animi? Velit delectus rem exercitationem sed!" />
-    
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+      <Post 
+      author="Jhonattan ferri" 
+      content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum consectetur fugiat aperiam tenetur ipsa! Consequuntur temporibus mollitia assumenda minus culpa excepturi necessitatibus quas nulla animi? Velit delectus rem exercitationem sed!" />
+      <Post 
+      author="Claro show"
+      content=" minus culpa excepturi necessitatibus quas nulla animi? Velit delectus rem exercitationem sed!" />
+      </main>
+    </div>
     </div>
   )
 }
