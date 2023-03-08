c4a1566352f423a00d513656051bbb4b833e8e49
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-03-07 22:55:59 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import {Post} from "./Post";
 export function App() {
   return (
     <div>
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
+    <Post 
+    author="Jhonattan ferri" 
+    content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum consectetur fugiat aperiam tenetur ipsa! Consequuntur temporibus mollitia assumenda minus culpa excepturi necessitatibus quas nulla animi? Velit delectus rem exercitationem sed!" />
+    <Post 
+    author="Claro show"
+    content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum consectetur fugiat aperiam tenetur ipsa! Consequuntur temporibus mollitia assumenda minus culpa excepturi necessitatibus quas nulla animi? Velit delectus rem exercitationem sed!" />
+    
     </div>
   )
 }
