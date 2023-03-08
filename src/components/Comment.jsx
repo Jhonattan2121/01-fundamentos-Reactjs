db44f38456212982b13abada5f4e82ff1253efcc
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-03-08 07:23:48 -0300

    Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/jhonattan2121.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Jhonattan Ferri</strong>
+                            <time title='08 de Março às 03:03' dateTime='2023-03-08 03:03:30'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp  />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+
+    )
+}
